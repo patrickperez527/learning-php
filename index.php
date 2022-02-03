3ac8ed9bdb416519fbd66ba5ff7b987ba7142eb0
John Patrick C. Perez <patrickperez527@gmail.com>
2022-02-03 21:50:53 +0800

    get data from database

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 
-    // connect to database 
+    // connect to database
     $conn = mysqli_connect('localhost', 'patrick', 'test1234', 'project1');
 
     // check connection
@@ -8,6 +8,22 @@
         echo 'Connection error: ' . mysqli_connect_error();
     }
 
+    // make query
+    $sql = 'SELECT title, email, ingredients FROM pizzas ORDER BY created_at';
+
+    // make query and get result
+    $result = mysqli_query($conn, $sql);
+
+    // fetch resulting rows as an array
+    $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    // free result from memory
+    mysqli_free_result($result);
+
+    // close connection
+    mysqli_close($conn);
+
+    print_r($pizzas);
 
 ?>
 
