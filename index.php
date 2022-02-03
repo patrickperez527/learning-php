c4553e41f50c88598b1a57da97d48b21c28856b5
John Patrick C. Perez <patrickperez527@gmail.com>
2022-02-03 21:31:24 +0800

    connecting to database

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php 
 
+    // connect to database 
+    $conn = mysqli_connect('localhost', 'patrick', 'test1234', 'project1');
+
+    // check connection
+    if (!$conn) {
+        echo 'Connection error: ' . mysqli_connect_error();
+    }
+
 
 ?>
 
