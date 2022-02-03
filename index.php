6d3af7395b310ec46871966edefb07f3330d7e52
John Patrick C. Perez <patrickperez527@gmail.com>
2022-02-03 22:19:13 +0800

    rendered data to the browser

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     }
 
     // make query
-    $sql = 'SELECT title, email, ingredients FROM pizzas ORDER BY created_at';
+    $sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
 
     // make query and get result
     $result = mysqli_query($conn, $sql);
@@ -23,7 +23,7 @@
     // close connection
     mysqli_close($conn);
 
-    print_r($pizzas);
+    // print_r($pizzas);
 
 ?>
 
@@ -32,6 +32,24 @@
 
     <?php include 'templates/header.php'; ?>
 
+    <h4 class="center grey-text">Pizzas!</h4>
+        <div class="container">
+            <div class="row">
+                <?php foreach($pizzas as $pizza) {?>
+                    <div class="col s6 md3">
+                        <div class="card z-depth-0">
+                            <div class="card-content center">
+                                <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+                                <div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+                            </div>
+                            <div class="card-action right-align">
+                                <a href="#" class="brand-text">More info</a>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
 
     <?php include 'templates/footer.php'; ?>
     
