3f84ecbaf4cbc54fd9a984e8166a6aee88a5eccb
John Patrick C. Perez <patrickperez527@gmail.com>
2022-02-04 10:51:36 +0800

    endforeach endif

--- index.php
+++ index.php
@@ -35,21 +35,28 @@
     <h4 class="center grey-text">Pizzas!</h4>
         <div class="container">
             <div class="row">
-                <?php foreach($pizzas as $pizza) {?>
+                <?php foreach($pizzas as $pizza) : ?>
                     <div class="col s6 md3">
                         <div class="card z-depth-0">
                             <div class="card-content center">
                                 <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
-                                <ul><?php foreach(explode(',', $pizzas[0]['ingredients']) as $ingredient) { ?></ul>
-                                <li><?php echo htmlspecialchars($ingredient); ?></li>
-                                <?php } ?>
+                                <ul><?php foreach(explode(',', $pizzas[0]['ingredients']) as $ingredient) : ?></ul>
+                                    <li><?php echo htmlspecialchars($ingredient); ?></li>
+                                    <?php endforeach ?>
                             </div>
                             <div class="card-action right-align">
                                 <a href="#" class="brand-text">More info</a>
                             </div>
                         </div>
                     </div>
-                <?php } ?>
+                <?php endforeach; ?>
+
+                <?php if(count($pizzas) >= 3) : ?>
+                    <p>There are 3 or more pizzas</p>
+                <?php else : ?>
+                    <p>There are less than 3 pizzas</p>
+                <?php endif; ?>
+
             </div>
         </div>
 
