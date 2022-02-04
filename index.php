c25cfbd4538db38dbe56031f5026811f14c4c8c4
John Patrick C. Perez <patrickperez527@gmail.com>
2022-02-04 14:13:44 +0800

    finished the project

--- index.php
+++ index.php
@@ -32,6 +32,7 @@
                 <?php foreach($pizzas as $pizza) : ?>
                     <div class="col s6 md3">
                         <div class="card z-depth-0">
+                            <img src="img/pizza.svg" alt="pizza" class="pizza">
                             <div class="card-content center">
                                 <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
                                 <ul><?php foreach(explode(',', $pizza['ingredients']) as $ingredient) : ?></ul>
