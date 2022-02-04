b2953adebba427ed2cfc1e64c614127d79298444
John Patrick C. Perez <patrickperez527@gmail.com>
2022-02-04 14:08:12 +0800

    added delete button

--- details.php
+++ details.php
@@ -2,6 +2,20 @@
 
     include 'config/db_connect.php';
 
+    if (isset($_POST['delete'])) {
+        $id_to_delete = mysqli_escape_string($conn, $_POST['id_to_delete']);
+
+        $sql = "DELETE FROM pizzas WHERE id = $id_to_delete";
+
+        if (mysqli_query($conn, $sql)) {
+            // success
+            header('Location: index.php');
+        } else {
+            // fail
+            echo 'Query error: ' . mysqli_error($conn);
+        }
+    }
+
     // check get request id parameter
     if (isset($_GET['id'])) {
         $id = mysqli_real_escape_string($conn, $_GET['id']);
@@ -38,6 +52,12 @@
             <p><?php echo date($pizza['created_at']); ?></p>
             <h5>Ingredients:</h5>
             <p><?php echo htmlspecialchars($pizza['ingredients']); ?></p>
+
+            <!-- DELETE FORM -->
+            <form action="details.php" method="POST">
+                <input type="hidden" name="id_to_delete" value="<?php echo $pizza['id']; ?>">
+                <input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
+            </form>
         <?php else: ?>
 
             <h5>No such pizza exists!</h5>
