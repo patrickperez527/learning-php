fe5914aa6e04885aea4e9e8965f99d2ee92183ac
John Patrick C. Perez <patrickperez527@gmail.com>
2022-02-05 20:02:01 +0800

    last

--- sandbox.php
+++ sandbox.php
@@ -4,30 +4,39 @@
 
     class User {
 
-        public $name;
-        public $email;
+        private $name;
+        private $email;
 
         public function __construct($name, $email){
-            // $this->name = 'mario';
-            // $this->email = 'test@yahoo.com';
             $this->name = $name;
             $this->email = $email;
         }
 
-
         public function login(){
             echo $this->name . ' logged in';
         }
+
+        public function getName(){
+            return $this->name;
+        }
+
+        public function setName($name){
+            if (is_string($name) && strlen($name) > 1) {
+                $this->name = $name;
+                return "the name has been update to $name";
+            } else {
+                echo 'not a valid name';
+            }
+        }
     }
 
-    // $userOne = new User();
-    // $userOne->login();
-    // echo $userOne->name;
-    // echo $userOne->email;
 
     $userTwo = new User('mario', 'test@yahoo.com');
-    echo $userTwo->name . '<br/>';
-    echo $userTwo->email . '<br/>';
-    $userTwo->login();
+
+    // echo $userTwo->getName();
+    // echo $userTwo->setName(50);
+    echo $userTwo->setName('shaun');
+    echo $userTwo->getName();
+
 
 ?>
